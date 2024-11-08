6b5efacc1a36353d659a5bf846a6f5114f934739
{nombre_de_usuario} <{correo_electronico}>
2024-11-08 16:45:48 -0300

    text compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/sebasechazu',
   base: '/axa.estudioaduanero',
-  integrations: [tailwind()]
-
+  integrations: [
+    tailwind(),
+    compress({
+      // Opciones de configuración
+      CSS: true,
+      HTML: true,
+      JavaScript: true,
+      Image: false,
+      SVG: false
+    })
+  ]
 });
