ed413a3ef94a1e0834ab18f247808cec73a9d766
Sebastián Echazú <sebastianechazu@outlook.com>
2024-11-24 15:02:24 -0300

    refactor: reestructura componentes de encabezado y elimina archivos obsoletos; actualiza importaciones y datos en secciones About, Services, Processes, FAQ y Testimonials

---
+++ src/data/data.ts
@@ -0,0 +1,172 @@
+//#region Hero
+export const hero = {
+    title: "Servicios de Comercio Exterior",
+    description:
+        " Nos especializamos en despacho aduanero, asesoría en comercio exterior, y gestión de documentación para que su empresa cumpla con todas las normativas y optimice sus costos de importación y exportación.",
+    button: "Contáctanos",
+    image: "/src/assets/hero.webp",
+};
+//#endregion Hero
+//#region About
+export const about = {
+    image: "https://images.unsplash.com/photo-1531973576160-7125cd663d86",
+    title: "Sobre Nosotros",
+    description:
+        "Somos una empresa de comercio exterior con más de 10 años de experiencia en el mercado. Nuestro equipo de profesionales está comprometido en brindar un servicio de calidad y asesoramiento personalizado a cada uno de nuestros clientes.",
+    button: "Contáctanos",
+};
+//#endregion About
+//#region Service
+export const headerServices = {
+    title: "Servicios que ofrecemos",
+    subtitle: "Nuestros Servicios",
+    description:
+        "Ofrecemos una amplia gama de servicios de comercio exterior para satisfacer las necesidades de nuestros clientes.",
+};
+export const services = [
+    {
+        title: "Despacho Aduanero",
+        description:
+            "Nos ocupamos de todos los aspectos del despacho aduanero, asegurando  que sus mercancías cumplan con la normativa vigente y lleguen a destino sin retrasos ni inconvenientes.",
+        img: "/src/assets/1.webp",
+    },
+    {
+        title: "Asesoramiento en Comercio Exterior",
+        description:
+            "Ofrecemos un asesoramiento integral en comercio exterior para optimizar sus procesos de importación y exportación, maximizando la eficiencia y reduciendo los costos operativos.",
+        img: "/src/assets/2.webp",
+    },
+    {
+        title: "Gestión de Documentación",
+        description:
+            "Administramos y gestionamos toda la documentación necesaria, facilitando un proceso rápido y cumpliendo con los requisitos legales para importaciones y exportaciones.",
+        img: "/src/assets/3.webp",
+    },
+];
+//#endregion Service 
+//#region Testimonials
+export const headerTestimonials = {
+    title: "Lo que dicen nuestros clientes",
+    subtitle: "Testimonios",
+    description:
+        "Estos son algunos de los comentarios que nuestros clientes han compartido sobre su experiencia con nosotros.",
+};
+
+export const testimonials = [
+    {
+        name: "Carlos Sánchez",
+        role: "Empresario",
+        message:
+            "Estoy muy contento con el servicio y los productos. La calidad es excelente y el precio inmejorable.",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+    },
+    {
+        name: "María Gómez",
+        role: "Deportista",
+        message:
+            "El equipo de esta estudio es muy profesional. Recomiendo sus servicios al 100%.",
+        image: "https://randomuser.me/api/portraits/women/2.jpg",
+    },
+    {
+        name: "Luis Fernández",
+        role: "Entrenador personal",
+        message:
+            "El servicio al cliente es fantástico, y los productos son justo lo que buscaba para mi entrenamiento.",
+        image: "https://randomuser.me/api/portraits/men/3.jpg",
+    },
+];
+//#endregion Testimonials
+//#region Process
+export const headerProcesses = {
+    title: "Nuestro Proceso",
+    subtitle: "Cómo trabajamos",
+    description:
+        "Te acompañamos en cada etapa del proceso aduanero, desde la consulta inicial hasta la gestión y seguimiento de tus trámites.",
+};
+export const processes = [
+    {
+        title: "Consulta Inicial",
+        description:
+            "Realizamos una primera consulta para entender tus necesidades y evaluar las mejores opciones de aduana.",
+        number: "1",
+    },
+    {
+        title: "Documentación",
+        description:
+            "Te asistimos en la preparación y revisión de toda la documentación necesaria para tus trámites aduaneros.",
+        number: "2",
+    },
+    {
+        title: "Gestión y Seguimiento",
+        description:
+            "Nos encargamos de la gestión de los trámites y realizamos el seguimiento para asegurar el cumplimiento en cada etapa.",
+        number: "3",
+    },
+];
+//#endregion Process
+//#region FAQ
+export const headerFAQ = {
+    title: "Preguntas Frecuentes",
+    subtitle: "FAQ",
+    description:
+        "Encuentra respuestas a las preguntas más comunes sobre el proceso aduanero y cómo podemos ayudarte.",
+};
+export const question = [
+    {
+        question: "¿Cuánto tiempo toma el proceso de importación/exportación?",
+        answer:
+            "El tiempo de importación o exportación depende de factores como el tipo de mercancía y los requisitos del país de destino. Nuestro equipo agiliza el proceso para reducir el tiempo y garantizar que sus mercancías lleguen a tiempo.",
+        type: "text a",
+    },
+
+    {
+        question: "¿Cuál es el costo de los servicios de transporte aduanero?",
+        answer:
+            "El costo de los servicios de transporte aduanero varía según el tipo de mercancía, el destino y los requisitos aduaneros. Nuestro equipo le proporcionará un presupuesto detallado y transparente para sus necesidades de transporte aduanero.",
+        type: "text b",
+    },
+    {
+        question: "¿Qué documentos se requieren para el proceso de importación/exportación?",
+        answer:
+            "Los documentos necesarios para el proceso de importación/exportación varían según el tipo de mercancía y el país de destino. Nuestro equipo le guiará a través de los requisitos y le ayudará a recopilar la documentación necesaria para el proceso aduanero.",
+        type: "text a",
+    },
+];
+//#endregion FAQ
+//#region Contact
+export const headerContact = {
+    title: "Contáctanos",
+    subtitle: "Estamos Aquí Para Ayudarte",
+    description:
+        "Si tienes alguna pregunta, no dudes en comunicarte con nosotros. Nuestro equipo está disponible para ayudarte y brindarte toda la información que necesites.",
+};
+
+export const ownerContact = {
+    name: "Alberto Aldabe",
+    role: "Agente de transporte Aduanero - Despachante de Aduana",
+    image: "https://randomuser.me/api/portraits/men/9.jpg"
+};
+
+export const datacontact = [
+    {
+        title: "Direccion",
+        description: "Bolivar 382 6to piso - Oficina 23 - Buenos Aires - Argentina - CP-1066",
+        icon: "M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418",
+    },
+    {
+        title: "Telefono",
+        description: "+54 (011) 45237962",
+        icon: "M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z",
+    },
+    {
+        title: "Email",
+        description: "alberto.aldabe@axacomex.com.ar",
+        icon: "M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75",
+    },
+    {
+        title: "Horas de Contacto",
+        description: "Monday - Friday: 9am to 8pm / Saturday: 10am to 6pm",
+        icon: "M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z",
+    }
+]
+//#endregion Contact
